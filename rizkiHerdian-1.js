d3a300bfacc4be22b80218c73d82fac5b813ba3a
Rizkiherdian03 <114628065+Rizkiherdian03@users.noreply.github.com>
2023-10-05 21:45:42 +0700

    Add files via upload

---
+++ rizkiHerdian-1.js
@@ -0,0 +1,10 @@
+let fisrtName = 'M Rizki'
+let lastName = 'Herdian'
+let age = 19
+let isMaried = 'false'
+
+
+console.log(fisrtName)
+console.log(lastName)
+console.log(age)
+console.log(isMaried)
